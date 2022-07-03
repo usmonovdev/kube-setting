5d058875cd15a9beaacdfb5cabce0ee4c1f2e469
usmonovdev <usmonovazizbek15@gmail.com>
2022-07-03 12:16:34 +0500

    structure

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 let container = document.getElementById("div")
 let externalHTML = `
-    <div class="front">Lorem ispum</div>
+    <div class="father">
+        <div class="child front">Front</div>
+        <div class="child back">Back</div>
+        <div class="child right">Right</div>
+        <div class="child left">Left</div>
+        <div class="child bottom">Bottom</div>
+        <div class="child top">Top</div>
+    </div>
+
+    <div>
+        <input type="range" id="x">
+        <label for="x">x deg</label>
+    </div>
+    <div>
+        <input type="range" id="y">
+        <label for="y">y deg</label>
+    </div>
+    <div>
+        <input type="range" id="z">
+        <label for="z">z deg</label>
+    </div>
 `;
 
 container.innerHTML = externalHTML;
